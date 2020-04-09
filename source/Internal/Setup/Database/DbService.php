3d538cf6b1c3cdfe932e77efad1b3ad21891035e
Abbas Afsharfarnia <afshar5152@gmail.com>
2020-04-09 11:48:27 +0200

    OXDEV-3209 Add DbService for Setup Command

---
+++ source/Internal/Setup/Database/DbService.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\EshopCommunity\Internal\Setup\Database;
+
+/**
+ * Class DbService
+ *
+ * @package OxidEsales\EshopCommunity\Internal\Setup\Database
+ */
+class DbService implements DbServiceInterface
+{
+
+    public function __construct()
+    {
+    }
+}
---
+++ tests/Integration/Internal/Setup/Database/DbServiceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Setup\Database;
+
+use OxidEsales\EshopCommunity\Tests\Integration\Internal\ContainerTrait;
+use PHPUnit\Framework\TestCase;
+
+class DbServiceTest extends TestCase
+{
+
+    use ContainerTrait;
+}
